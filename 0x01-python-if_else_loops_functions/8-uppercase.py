9aad7d805158ebe9d990fd1f9bdac90363670751
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-02 18:45:10 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= ord('A') and ord(c) <= ord('z'):
+        if ord(c) >= ord('A') and ord(c) <= ord('Z'):
             print("{:c}".format(ord(c)), end="")
         else:
             print("{:c}".format(ord(c) - 32), end="")
