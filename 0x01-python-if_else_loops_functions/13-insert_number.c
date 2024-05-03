f13939e30d9200ed3b656cec926b618461e849fe
Ethar7 <nabihethar@yahoo.com>
2024-05-03 01:56:45 -0700

    o

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,24 @@
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *newnode = malloc(sizeof(listint_t));
+	if (!newnode)
+		return (NULL);
+	newnode->n = number;
+	newnode->next = NULL;
+	if (!node || newnode->n < node->n)
+	{
+		newnode->next = node;
+		return (*head = newnode);
+	}
+	while(node)
+	{
+		if (!node->next || newnode->n < newnode->next->n)
+		{
+			newnode->next = node->next;
+			node->next = newnode;
+			return (node);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}			
