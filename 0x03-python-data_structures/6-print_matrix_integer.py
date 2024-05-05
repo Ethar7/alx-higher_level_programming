fb64ddcf909ab9b49cfbf8ef6a3f4970d323ab7e
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-05 21:28:39 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix or matrix == [[]]:
-        return None
+    if not matrix or matrix == [[]]:  # Check if matrix is empty or contains an empty list
+        return
     for row in matrix:
-        for i in range(len(row) - 1):
+        for i in range(len(row)):
             if i == len(row) - 1:
-                print("{:d}".format(row[i]), end="\n")
+                print("{:d}".format(row[i]), end="\n")  # Print the last element with a newline
             else:
-                print("{:d}".format(row[i]), end=" ")
+                print("{:d}".format(row[i]), end=" ")  # Print other elements with a space
