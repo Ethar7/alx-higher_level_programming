1c8be4b32fa1d2e5170e07313f15a9cd58a149a2
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-06 16:06:02 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda num: list(map(lambda e: e ** 2, num)), matrix))
+    return list(map(lambda row: list(map(lambda e: e ** 2, row)), matrix))
