5065a3e03625da3360566e13f39bcb062e3df53d
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-06 19:23:16 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return sum(set(my_list))
+    seen = set()
+    def is_first_occurrence(x):
+        if x not in seen:
+            seen.add(x)
+    unique_elements = filter(is_first_occurrence, my_list)
+    return sum(unique_elements)
