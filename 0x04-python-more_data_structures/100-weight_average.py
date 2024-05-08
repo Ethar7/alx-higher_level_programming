b59aad656ac0b8e2de77c968299efde394495eeb
Ethar7 <nabihethar@yahoo.com>
2024-05-08 15:58:50 -0700

    o

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    for score, weight in my_list:
+        numerator = sum(score * weight)
+    denominator = sum(weight for _, weight in my_list)
+
+    if denominator == 0:
+        return 0
+
+    return numerator / denominator
+
