2cf7813c3a105d8f8185af2615bc64309d2a54d2
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-14 10:28:28 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,6 +39,7 @@ class Square:
 
     @position.setter
     def position(self, value):
+        """Set the position of the square, ensuring it's a tuple of 2 positive integers."""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) and num >= 0 for num in value)):
