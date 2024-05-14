29d91ea9c72c908480fa72f9e508ae0635502a3c
Ethar7 <nabihethar@yahoo.com>
2024-05-13 22:00:04 -0700

    i

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,9 +5,9 @@ and a method to calculate the area of the square.
 """
 
 
-
 class Square:
-    """Square class: Represents a square with a private instance attribute 'size'."""
+    """Square class: Represents a square with a private
+    instance attribute 'size'."""
 
     def __init__(self, size=0):
         """
