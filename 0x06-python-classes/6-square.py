8bba3cc689ec779fdaeb3d895897cc7250b4fcbf
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-14 09:43:14 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,40 +6,29 @@ and a method to calculate the area of the square.
 
 
 class Square:
-    """Square class: Represents a square with a private
-    instance attribute 'size'."""
-
     def __init__(self, size=0, position=(0, 0)):
-
-    self.__size = size
-    self.__position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
-        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square.
-
-        Args:
-            value (int): The new size of the square.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return self.__position
+
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or
+        if not isinstance(value, tuple) or len(value) != 2 or \
            not all(isinstance(num, int) and num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
