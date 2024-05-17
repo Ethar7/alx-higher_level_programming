cb8b896485e25735a2470f18341d94941e503934
Ethar7 <nabihethar@yahoo.com>
2024-05-16 19:19:02 -0700

    o

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" this is module documentation"""
+
+
+def add_integer(a, b=98):
+    """
+    Function to add two nums a, b
+    if a, b float it casted to int
+    else raise type error
+
+    Args:
+    a: int or float
+    b: int or float
+
+    Returns:
+    an int sum
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
