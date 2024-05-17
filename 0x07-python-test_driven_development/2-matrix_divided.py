7d124f78f85823c93d50ae0f950bcdb1f54bc5d7
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-17 13:01:44 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,7 +13,9 @@ def matrix_divided(matrix, div):
     Returns:
     A new matrix with the division results.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
+    if not isinstance(matrix, list) or 
+    not all(isinstance(row, list) for row in matrix) 
+    or not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -24,3 +26,7 @@ def matrix_divided(matrix, div):
     
     return [[round(elem / div, 2) for elem in row] for row in matrix]
 
+
+if __name__ == "main":
+    import doctest
+    doctest.testfile(tests/2-matrix_divided.txt)
