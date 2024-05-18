7d78b3bd3f386f407bd9c8d8c85463891df186f7
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-18 15:18:17 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -20,5 +20,7 @@ def text_indentation(text):
         result += char
         if char in ".?:":
             result += "\n\n"  # Add 2 new lines after the character
+        elif char == " ":
+            result += ""
 
     print(result, end="")
