3d0bd0f6aeb09506567b683de8715cf0f01e62f3
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-20 15:53:30 +0300

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,7 +3,8 @@
 
 
 class Rectangle:
-    """A class that defines a rectangle with private width and height attributes."""
+    """A class that defines a rectangle with 
+    private width and height attributes."""
 
     number_of_instances = 0
 
@@ -20,7 +21,8 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle, ensuring it is an integer and non-negative."""
+        """Set the width of the rectangle, 
+        ensuring it is an integer and non-negative."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -34,7 +36,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle, ensuring it is an integer and non-negative."""
+        """Set the height of the rectangle,
+        ensuring it is an integer and non-negative."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -46,7 +49,8 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle, or 0 if width or height is 0."""
+        """Return the perimeter of the 
+        rectangle, or 0 if width or height is 0."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
@@ -61,11 +65,13 @@ class Rectangle:
         return "\n".join(rectangle_str)
 
     def __repr__(self):
-        """Return a string representation for recreating the instance."""
+        """Return a string representation 
+        for recreating the instance."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        """Print a message when an instance of Rectangle is deleted and decrement the instance counter."""
+        """Print a message when an instance of Rectangle is deleted
+        and decrement the instance counter."""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
 
