8eaa2071d0cab88535dc05339e4bc4f65122865d
Ethar7 <nabihethar@yahoo.com>
2024-05-23 10:09:55 -0700

    o

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is module documentation"""
+
+
+class BaseGeometry:
+    """this is doc for class"""
+    def area(self):
+        raise Exception("area() is not implemented")
