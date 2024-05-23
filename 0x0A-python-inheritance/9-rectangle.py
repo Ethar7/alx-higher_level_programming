1818918728cf636c8c584ccdccfb6998a7464156
Ethar7 <nabihethar@yahoo.com>
2024-05-23 11:03:55 -0700

    o

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this is a module documentation"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
