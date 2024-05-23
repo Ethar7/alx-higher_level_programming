54bd4ab6a2808cb4f3263b54182e32c597cb8156
Ethar7 <nabihethar@yahoo.com>
2024-05-23 09:29:58 -0700

    0-lookup.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """this is function doc"""
 
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
