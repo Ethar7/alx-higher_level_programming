b008f8b557b3e4b881bc000f9f519615e62891a5
Ethar7 <nabihethar@yahoo.com>
2024-05-23 02:10:52 -0700

    o

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is module documentation"""
+
+
+class LockedClass:
+    """this defines that only first name 
+    attribute was allowed"""
+
+    __slots__ = ("first_name")
