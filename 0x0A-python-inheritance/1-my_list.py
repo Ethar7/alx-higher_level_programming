441de98f52b3f127e12dbcb2e6107ef0cc154236
Ethar7 <nabihethar@yahoo.com>
2024-05-23 08:04:40 -0700

    p

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this is the module documentation"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
