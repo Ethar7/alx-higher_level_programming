2d4240a6984b321cc8375887cab6ff147904408e
Ethar7 <nabihethar@yahoo.com>
2024-05-23 09:25:38 -0700

    o

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is module documentation"""
+
+
+def inherits_from(obj, a_class):
+    """this is function doc"""
+
+    return isinstance(obj, a_class) and type(obj) != a_class
