b2fcbb479669b32440b91ef7cb593c3e3f31ccfe
Ethar7 <nabihethar@yahoo.com>
2024-05-24 05:37:53 -0700

    o

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this is module doc"""
+
+
+import sys
+load_from_json_file = _import_('6-load_from_json_file').load_from_json_file
+save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
+
+    filename = "add_item.json"
+    args = sys.argv[1:]  # Exclude the script name from the arguments
+
+    # Load existing data or create an empty list
+    my_list = load_from_json_file(filename)
+
+    # Add command-line arguments to the list
+    my_list.extend(args)
+
+    # Save the updated list to the file
+    save_to_json_file(my_list, filename)
+~                                                                                                                                                       
+~                                                                                                                                                       
+~
