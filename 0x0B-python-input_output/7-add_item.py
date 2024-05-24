b0bfcfa8080f65a41a0682cee5dc79003e7faa18
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-24 15:48:53 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,15 @@ import sys
 from _import_('6-load_from_json_file').load_from_json_file
 from _import_('5-save_to_json_file').save_to_json_file
 
-def main():
-    filename = "add_item.json"
-    args = list(sys.argv[1:])  # Exclude the script name from the arguments
-    try:
-        # Load existing data or create an empty list
-        my_list = load_from_json_file(filename)
-    except:
-        my_list = []
-    # Add command-line arguments to the list
-    my_list.extend(args)
-    # Save the updated list to the file
-    save_to_json_file(my_list, filename)
+
+filename = "add_item.json"
+args = list(sys.argv[1:])  # Exclude the script name from the arguments
+try:
+    # Load existing data or create an empty list
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+# Add command-line arguments to the list
+my_list.extend(args)
+ # Save the updated list to the file
+save_to_json_file(my_list, filename)
