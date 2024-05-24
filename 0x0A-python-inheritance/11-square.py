81b0dae3b791befb8c1cc4d58f86c41f9b0693aa
Ethar7 <nabihethar@yahoo.com>
2024-05-23 18:11:22 -0700

    o

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this is module documentation"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square using Rectangle as base class."""
+
+    def __init__(self, size):
+        """Initialize a new Square."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return a string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
