ca62a989eed3f1030c6e463a465651a5ad8bab1f
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-24 15:50:39 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """This is a module for adding command-line arguments to a list and saving it as JSON."""
 
+
 import sys
 from _import_('6-load_from_json_file').load_from_json_file
 from _import_('5-save_to_json_file').save_to_json_file
-
 
 filename = "add_item.json"
 args = list(sys.argv[1:])  # Exclude the script name from the arguments
 try:
     # Load existing data or create an empty list
     my_list = load_from_json_file(filename)
-except:
+except Exception:
     my_list = []
 # Add command-line arguments to the list
 my_list.extend(args)
