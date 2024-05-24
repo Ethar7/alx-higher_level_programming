4c8b05937cf291ff95b842c3284f7a0858223bb0
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-05-24 19:08:32 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -27,31 +27,6 @@ class Student:
         Returns:
             dict: A dictionary containing the specified attributes and their values.
         """
-    class Student:
-    def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student object with the given attributes.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of the Student instance.
-
-        Args:
-            attrs (list, optional): List of attribute names to include in the dictionary.
-                If None, all attributes are included.
-
-        Returns:
-            dict: A dictionary containing the specified attributes and their values.
-        """
         if attrs is None:
             return self.__dict__
         else:
